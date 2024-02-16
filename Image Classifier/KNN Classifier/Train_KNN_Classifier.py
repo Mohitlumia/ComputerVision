99843884acd81d069e23136e625634f4ca9f4227
Mohit Gupta <mohitproduct@gmail.com>
2024-02-16 11:56:47 +0530

    initial commit

--- Image Classifier/KNN Classifier/Train_KNN_Classifier.py
+++ Image Classifier/KNN Classifier/Train_KNN_Classifier.py
@@ -1,9 +1,20 @@
 
-import os
-from skillsnetwork import cvstudio
+import json
+import random
+import cv2
 
-# Initialize the CV Studio Client
-cvstudioClient = cvstudio.CVStudio()
+# loading a json file with annotations of dog and cat images
+jsonFile = open("Image Classifier/dog and cat images and annotation/_annotations.json")
+annotations = json.load(jsonFile)
 
-# Download All Images
-cvstudioClient.downloadAll()
+# randomly choosing an image
+random_filename = 'Image Classifier/dog and cat images and annotation/' + random.choice(list(annotations["annotations"].keys()))
+
+# reading and resizing to reduce the processing time
+image = cv2.imread(random_filename)
+image = cv2.resize(image, (32, 32))
+
+# and now show the image
+cv2.imshow("Example Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
