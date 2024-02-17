95dd983fa1375be721699c84e67ed7b177c252ce
Mohit Gupta <mohitproduct@gmail.com>
2024-02-17 12:38:31 +0530

    get result accuracy by confusion_matrix

--- Image Classifier/KNN Classifier/Train_KNN_Classifier.py
+++ Image Classifier/KNN Classifier/Train_KNN_Classifier.py
@@ -41,23 +41,23 @@ train_labels = np.array(train_labels)
 train_labels = train_labels.astype(int)
 train_labels = train_labels.reshape((train_labels.size,1))
 
+########################################################################
+
+from sklearn.model_selection import train_test_split
 
 # split samples and labels for training and testing
 test_size = 0.2
-train_samples = train_images[:800]
-train_labels = train_labels[:800]
-test_samples = train_images[800:]
-test_labels = train_labels[800:]
+train_samples, test_samples, train_labels, test_labels = train_test_split(
+    train_images, train_labels, test_size=test_size, random_state=0)
 
 # KNN works on the majority votes from k nearest sample
 # k is a hyper-perameter which can be found with max accuracy in Validation Table
-
 # to train the KNN model we will us cv2.ml.KNearest_create() from cv2
 
 knn = cv2.ml.KNearest_create()
 knn.train(train_samples, cv2.ml.ROW_SAMPLE, train_labels)
 
-## get different values of K
+# get different values of K
 k_values = [1, 2, 3, 4, 5]
 k_result = []
 for k in k_values:
@@ -68,3 +68,24 @@ for res in k_result:
     flat_result = [item for sublist in res for item in sublist]
     flattened.append(flat_result)
 
+
+########################################################################
+from sklearn.metrics import confusion_matrix
+
+# create an empty list to save accuracy and the cofusion matrix
+accuracy_res = []
+con_matrix = []
+# we will use a loop because we have multiple value of k
+for k_res in k_result:
+    label_names = [0, 1]
+    cmx = confusion_matrix(test_labels, k_res, labels=label_names)
+    con_matrix.append(cmx)
+    # get values for when we predict accurately
+    matches = k_res==test_labels
+    correct = np.count_nonzero(matches)
+    # calculate accuracy
+    accuracy = correct*100.0/result.size
+    accuracy_res.append(accuracy)
+# stor accuracy for later when we create the graph
+res_accuracy = {k_values[i]: accuracy_res[i] for i in range(len(k_values))}
+list_res = sorted(res_accuracy.items())
