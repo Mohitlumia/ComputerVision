8231c3e39c76793bbbe46401982e057227cd98a1
Mohit Gupta <mohitproduct@gmail.com>
2024-03-06 08:13:13 +0530

    updates

--- Image Classifier/Softmax Classifier/MNIST_Classification_with_softmax.py
+++ Image Classifier/Softmax Classifier/MNIST_Classification_with_softmax.py
@@ -1,28 +1,20 @@
 
-##########################################################
-# download the Training and Validation MNIST digit images
-
 
 import torchvision.datasets as dsets        # Allows us to get the digit dataset
 import torchvision.transforms as transforms # Allows us to transform data
+import torch.nn as nn                       # PyTorch Neural Network
+import torch.optim, torch.utils
+from matplotlib import pyplot as plt
 
+##########################################################
+# download the Training and Validation MNIST digit images
 
 train_dataset = dsets.MNIST(root='./Image Classifier/MNIST', train=True, download=True, transform=transforms.ToTensor())
-print("Print the training dataset:\n ", train_dataset)
 
 validation_dataset = dsets.MNIST(root='./Image Classifier/MNIST', download=True, transform=transforms.ToTensor())
-print("Print the validation dataset:\n ", validation_dataset)
-
-print(train_dataset[3])     # print the image at index 3
-print(train_dataset[3][1])  # print the lable of the image at index 3
-
 
 ##########################################################
 # Create a Softmax Classifier using PyTorch
-
-
-import torch.nn as nn   # PyTorch Neural Network
-
 
 # Define softmax classifier class
 # Inherits nn.Module which is the base class for all neural networks
@@ -40,34 +32,17 @@ class SoftMax(nn.Module):
         z = self.linear(x)
         return z
 
-train_dataset = dsets.MNIST(root='Image Classifier/MNIST', train=True, download=True, transform=transforms.ToTensor())
-validation_dataset = dsets.MNIST(root='Image Classifier/MNIST', download=True, transform=transforms.ToTensor())
 
 # Set input size and output size
-input_dim = 28 * 28
-output_dim = 10
+input_dim = 28 * 28     #image size
+output_dim = 10         #number of class of images
 
 # Create the model
 # Input dim is 28*28 which is the image converted to a tensor
 # Output dim is 10 because there are 10 possible digits the image can be
 model = SoftMax(input_dim, output_dim)
 
-# First we get the X value of the first image
-X = train_dataset[0][0]
-
-# We can see the shape is 1 by 28 by 28, we need it to be flattened to 1 by 28 * 28 (784)
-X = X.view(-1, 28*28)
-
-# Now we can make a prediction, each class has a value, and the higher it is the more confident the model is that it is that digit
-# compare model output one first image with its label
-import torch
-
-model_output = model(X)
-actual = torch.tensor([train_dataset[0][1]])
-
-print("Output: ", model_output)
-print("Actual:", actual)
-
+#################################################################################
 # Define the learning rate, optimizer, criterion, and data loader
 
 learning_rate = 0.1
@@ -78,7 +53,6 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 # The criterion will measure the loss between the prediction and actual label values
 # This is where the SoftMax occurs, it is built into the Criterion Cross Entropy Loss
 criterion = nn.CrossEntropyLoss()
-print(criterion(model_output, actual))
 
 # Created a training data loader so we can set the batch size
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=100)
@@ -103,8 +77,10 @@ def train_model(n_epochs):
         for x, y in train_loader:
             # Resets the calculated gradient value, this must be done each time as it accumulates if we do not reset
             optimizer.zero_grad()
+            # flat the image
+            flat_x = x.view(-1, 28 * 28)
             # Makes a prediction based on the image tensor
-            z = model(x.view(-1, 28 * 28))
+            z = model(flat_x)
             # Calculates loss between the model output and actual class
             loss = criterion(z, y)
              # Calculates the gradient value with respect to each weight and bias
@@ -116,8 +92,10 @@ def train_model(n_epochs):
         correct = 0
         # For each batch in the validation loader
         for x_test, y_test in validation_loader:
+            # flat the test data
+            flat_x_test = x_test.view(-1, 28 * 28)
             # Makes prediction based on image tensor
-            z = model(x_test.view(-1, 28 * 28))
+            z = model(flat_x_test)
             # Finds the class with the higest output
             _, yhat = torch.max(z.data, 1)
             # Checks if the prediction matches the actual class and increments correct if it does
@@ -132,9 +110,8 @@ def train_model(n_epochs):
 # Function call
 train_model(n_epochs)
 
-
+####################################################################
 # Plot the loss and accuracy
-from matplotlib import pyplot as plt
 
 fig, ax1 = plt.subplots()
 color = 'tab:red'
